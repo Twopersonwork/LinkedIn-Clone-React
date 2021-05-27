0d8b4d1fb5b4f463978fa76b8c7b54631910aab3
prachijpatel <prachi.jpatel1@gmail.com>
2021-05-27 22:22:39 +0530

    changed in my network in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -115,7 +115,6 @@ class Header extends Component {
                     <Badge
                       classes={{ badge: classes.customBadge }}
                       style={{
-                        paddingLeft: "20px",
                         bottom: "20px",
                         left: "55px",
                       }}
@@ -124,9 +123,7 @@ class Header extends Component {
                           ? props.user.waitFollowers.length
                           : 0
                       }
-                    >
-                      {/* <span class="badge badge-pill yellow blue-text">0</span> */}
-                    </Badge>
+                    ></Badge>
                   )}
                 </UserContext.Consumer>
                 <Link onClick={this.thisClicked} to={"/network"}>
@@ -150,8 +147,15 @@ class Header extends Component {
                       <path d="m12 16v6h-9v-6c0-1.7 1.3-3 3-3h3c1.7 0 3 1.3 3 3zm5.5-3c1.9 0 3.5-1.6 3.5-3.5s-1.6-3.5-3.5-3.5-3.5 1.6-3.5 3.5 1.6 3.5 3.5 3.5zm1 2h-2c-1.4 0-2.5 1.1-2.5 2.5v4.5h7v-4.5c0-1.4-1.1-2.5-2.5-2.5zm-11-13c-2.5 0-4.5 2-4.5 4.5s2 4.5 4.5 4.5 4.5-2 4.5-4.5-2-4.5-4.5-4.5z"></path>
                     </svg>
                   )}
-
-                  <span onClick={this.thisClicked}>My Network</span>
+                  <NavList
+                    className={
+                      window.location.pathname.slice(1) == "network"
+                        ? "active"
+                        : null
+                    }
+                  >
+                    <span onClick={this.thisClicked}>My Network</span>
+                  </NavList>
                 </Link>
               </NavList>
 
